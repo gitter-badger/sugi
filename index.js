363d1a36d6b262c5a6e56514e06c728e3200f2f7
Kevin O'Neil <kevinmoneil@me.com>
2016-05-11 01:59:47 +0900

    tests jsdom

---
+++ index.js
@@ -0,0 +1,29 @@
+// Run some jQuery on a html fragment
+const marked = require('marked');
+const jsdom = require("jsdom");
+
+var test = marked('# header')
+jsdom.env({
+  html: test,
+  done: function (errors, window) {
+    document = window.document;
+    var header = document.getElementsByTagName('h1')[0];
+    console.log(header.textContent);
+  }
+});
+
+//
+//
+//
+// var marked = require('marked');
+// var jsdom = require('jsdom').jsdom;
+//
+// const example = marked('# Hi <br> ## Hello');
+// var test = '';
+// jsdom.env({
+//   html: '<h1>Hello</h1>',
+//   done: function(err, window) {
+//     test = window.document.body;
+//     logTest(test);
+//   }
+// });
