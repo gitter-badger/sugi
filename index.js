ebe76398d974f3d8947dce18bead65f46a4ae8b8
Kevin O'Neil <kevinmoneil@me.com>
2016-05-17 00:24:34 +0900

    adds in arguements for cli

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ require('./lib/parse.js')
 
 program
   .version('0.0.1')
-  .usage('<file ...>')
+  .arguments('<input> [output]')
+  .action(function(cmd, input, output) {
+    console.log(input);
+  })
+  .parse(process.argv)
