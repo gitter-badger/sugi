11637dee68c4481254ce5481adaa86f0abb2f141
Kevin O'Neil <kevinmoneil@me.com>
2016-05-28 21:00:54 +0900

    cleans up index file

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 
-"use strict";
-
 const program = require('commander');
 const parse = require('./lib/parse.js');
-
-let input;
 
 program
   .version('0.0.1')
   .arguments('<input> [output]')
-  .action(function(input, output) {
+  .action((input, output) => {
     parse.isInputAFile(input, output);
   })
   .parse(process.argv);
