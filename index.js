d39b07432b81722ad611022dd87f0ed290d284ca
Kevin O'Neil <kevinmoneil@me.com>
2016-06-01 23:23:50 +0900

    imports parsedHtml obj from parse file

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 const program = require('commander');
 const parse = require('./lib/parse.js').parse;
 const write = require('./lib/write-file.js').write;
-const sugi = {};
+const options = {}
 
 program
   .version('0.0.1')
   .arguments('<input> [output]')
   .action((input, output) => {
-    sugi.importFileName = input;
+    options.importFileName = input;
     parse.transformIntoHtml(input);
     write.writeToFile(input, output, parse.parsedHtml);
   })
   .parse(process.argv);
 
-exports.sugi = sugi;
+exports.options = options;
